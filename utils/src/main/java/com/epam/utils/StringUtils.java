cc846f00bae60f2289a60115ed869550226be11a
daniil-torubara <dabarara@bk.ru>
2023-10-21 20:47:51 +0600

    stage 2 module 1 gradle

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,9 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        double num = Double.parseDouble(str);
-        return num >= 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
 
