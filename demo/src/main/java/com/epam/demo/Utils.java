0cdf422d4e3113787e730f5e9e5514eed8dcefae
daniil-torubara <dabarara@bk.ru>
2023-10-21 20:12:29 +0600

    stage 2 module 1 gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,16 +5,6 @@ import java.*
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        boolean result = true;
-        for (String strNum: args) {
-            int num = Integer.valueOf(strNum);
-            if(num >= 0){
-                result = result && true;
-            }
-            else {
-                result = result && false;
-            }
-        }
-        return result;
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
